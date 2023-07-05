c73280c129c02baa2043beb06780e3443f7c4e6b
leetaeyu <ehdrkq2005@naver.com>
2023-07-05 11:19:47 +0900

    validation

--- js/common.js
+++ js/common.js
@@ -1,9 +1,4 @@
 $(function(){
-    
-    // $(document).ready(function(){
-        //     $(".input_item").attr("required", false);
-        // })
-        
     // modal
     const body = $("body");
     const modalClose = $(".modal_title_box .is-close");
@@ -22,70 +17,120 @@ $(function(){
         $(".information").removeClass("active");
     })
 
-    // validation
-    const chkEmail = $(".chkEmail");
-    const chkName = $(".chkName");
-    const chkPw = $(".chkPw");
-    const chkPhone = $(".chkPhone");
-    const chkNewPw = $(".chkNewPw");
+    // email validation
+    isEmailValidated = function(_this) {
+        let emailRegExp = /^[a-z0-9A-Z._-]+@[a-z0-9A-Z_-]+\.[a-zA-Z.]*$/i;
+        $(_this).val($(_this).val().trim());
 
-    let emailReg = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
-    let pwReg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-    let nameReg = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{2,}$/;
-    let phoneReg = /^(010{1})[0-9]{3,4}[0-9]{4}$/;
+        let element = $(_this);
+        let email = $(_this).val();
+        let inputWrap = element.closest('.input_wrap');
 
-    //email
-    chkEmail.keyup(function(){
-        if(!this.value.match(emailReg)) {
-            $(this).siblings("p").text("이메일 양식에 맞춰 입력해주세요.");
-            return false;
-        } 
-        $(this).siblings("p").text("");
-        return true;
-    })
-    //password
-    chkPw.keyup(function(){
-        if(!this.value.match(pwReg)) {
-            $(this).siblings("p").text("비밀번호는 8~32자 이며 1개 이상의 특수문자 입력이 필요합니다.");
-            return false;
-        }
-        $(this).siblings("p").text("");
-        return true;
-    })
-    //pw reset
-    chkNewPw.keyup(function(){
+        var chkB = email.replace(/[^A-Za-z0-9@._-]/gi, '');
+        $(_this).val(chkB);
+
+        if (email == "" || email.length < 1) {
+            $(_this).siblings("p").text("이메일 양식에 맞춰 입력해주세요.");
+			return false;
+		}
+		else if (email.length > 64) {
+            $(_this).siblings("p").text("이메일은 64자가 넘을 수 없습니다 .");
+		}
+		else if (!emailRegExp.test(email)) {
+			$(_this).siblings("p").text("이메일 양식에 맞춰 입력해주세요.");
+		}
+		else {
+			$(_this).siblings("p").text("");
+			return true;
+		}
+    }
+
+	// name validation
+	isNameValidated = function(_this){
+		let element = $(_this);
+		let inputWrap = element.closest('.input_wrap');
+		let name = $(_this).val();
+		var chkB = name.replace(/[^a-zA-Zㄱ-힣._-]/gi, '');
+		$(_this).val(chkB);
+		
+		if (name == "" || name.length <= 1) {
+			$(_this).siblings("p").text("이름을 입력해주세요.");
+			return false;
+		} else if (name.length > 4) {
+            $(_this).siblings("p").text("이름은 최대 4자까지 입력해주세요.");
+			return false;
+		} else {
+			$(_this).siblings("p").text("");
+			return true;
+		}
+	}
+
+    // password validation
+	isPwValidated = function(_this){
+		$(_this).val($(_this).val().trim());
+		var userPw = $(_this).val();
+		let element = $(_this);
+		let inputWrap = element.closest('.input_wrap');
+		let reg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/
+
+		let checkAsterisk = userPw.search(/[!@#$%^*+=-]/g);
+		let checkNumber = userPw.search(/[0-9]/g);
+		let checkEnglish = userPw.search(/[a-z]/ig);
+	
+		if (userPw.length > 32) {
+            $(_this).siblings("p").text("비밀번호는 최대 32자까지 입력해주세요.");
+			return false;
+		}
+		else if(checkAsterisk <0 || checkNumber <0|| checkEnglish<0){
+            $(_this).siblings("p").text("비밀번호는 8~32자 이며 1개 이상의 특수문자 입력이 필요합니다.");
+			return false;
+		}
+		else {
+			$(_this).siblings("p").text("");
+		}
+
+        //비밀번호 확인
+        const chkPw = $(".chkPw");
+        const chkNewPw = $(".chkNewPw");
         const newPw = chkPw.val();
         const newCheck = chkNewPw.val();
+
         if(newPw != newCheck){
-            $(this).siblings("p").text("비밀번호가 불일치합니다.");
+            $(chkNewPw).siblings("p").text("비밀번호가 불일치합니다.");
             return false;
         }
-        $(this).siblings("p").text("");
-        return true;
-    })
-    //name
-    chkName.keyup(function(){
-        if(!this.value.match(nameReg)) {
-            $(this).siblings("p").text("이름을 입력해주세요.");
-            return false;
-        }
-        $(this).siblings("p").text("");
-        return true;
-    })
-    //phone
-    chkPhone.keyup(function(){
-        if(!this.value.match(phoneReg)) {
-            $(this).siblings("p").text("연락처는 '-'를 제외하고 입력해 주세요.");
-            return false;
-        }
-        $(this).siblings("p").text("");
-        return true;
-    })
+        $(chkNewPw).siblings("p").text("");
+		return true;
+    
+	}
 
-    //비밀번호 재설정
+    //phone validation
+    isPhoneValidated = function(_this) {
+        let phoneRegExp = /^(010{1})[0-9]{3,4}[0-9]{4}$/i;
+        $(_this).val($(_this).val().trim());
+
+        let element = $(_this);
+        let phone = $(_this).val();
+        let inputWrap = element.closest('.input_wrap');
+
+        if (phone == "" || phone.length < 1) {
+            $(_this).siblings("p").text("연락처는 '-'를 제외하고 입력해 주세요.");
+			return false;
+		}
+		else if (phone.length > 11) {
+            $(_this).siblings("p").text("번호는 11자가 넘을 수 없습니다 .");
+		}
+		else if (!phoneRegExp.test(phone)) {
+			$(_this).siblings("p").text("연락처는 '-'를 제외하고 입력해 주세요.");
+		}
+		else {
+			$(_this).siblings("p").text("");
+			return true;
+		}
+    }
+
+    // password eye    
     const eye = $(".eye_img");
-    const eyeImg = $(".eye_img img")
-
     eye.on("click", function(e){
         $(this).find("img").attr("src", function (index, attr) {
             if (attr.match("_on")){
@@ -100,6 +145,7 @@ $(function(){
             $(this).siblings("input").attr("type", "password");
         }
     })
+
 })
 
 
